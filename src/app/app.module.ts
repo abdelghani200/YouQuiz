90dd97e16c541515c6b478bb687c6fa9355ef635
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-11-02 11:25:13 +0100

    les components et service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
