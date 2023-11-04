c8fbab6b10b5b9c0fe5830f45e4cbdf4462c4c06
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-11-04 16:04:05 +0100

    git commit -m "QA-8 component datatable created"

---
+++ src/app/components/subjects/datatable/datatable.component.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datatable',
+  templateUrl: './datatable.component.html',
+  styleUrls: ['./datatable.component.css']
+})
+export class DatatableComponent{
+  
+
+}
