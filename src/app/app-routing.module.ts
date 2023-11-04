c4ab69acfac9254178c782d4734a3f44388d2e27
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-11-04 16:08:13 +0100

    modifications & installations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormaddComponent } from './components/subjects/formadd/formadd.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { DatatableComponent } from './components/subjects/datatable/datatable.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StatisticsComponent},
+  { path: 'datatable', component: DatatableComponent},
+  { path: 'formadd', component: FormaddComponent },
+  { path: 'chart', component: ChartComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ResultsComponent } from './results/results.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ResultsComponent } from './components/results/results.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ChartComponent } from './components/chart/chart.component';
+import { DatatableComponent } from './components/subjects/datatable/datatable.component';
+import { FormaddComponent } from './components/subjects/formadd/formadd.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,11 +20,19 @@ import { ResultsComponent } from './results/results.component';
     SidebarComponent,
     FooterComponent,
     HomeComponent,
-    ResultsComponent
+    ResultsComponent,
+    ChartComponent,
+    DatatableComponent,
+    FormaddComponent,
+    StatisticsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxChartsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
