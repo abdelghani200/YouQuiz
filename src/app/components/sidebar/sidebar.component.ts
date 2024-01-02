26965742de61600a3b7f31fee4568e144139e06e
A.Jelouani <jelouaniabdelghani@gmail.com>
2024-01-02 14:58:49 +0100

    component statics

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  animations: [
-    trigger('enterLeaveAnimation', [
-      state('open', style({ transform: 'translateX(0)' })),
-      state('closed', style({ transform: 'translateX(-100%)' })),
-      transition('open => closed', animate('300ms ease-in-out')),
-      transition('closed => open', animate('300ms ease-in-out')),
-    ]),
-  ],
 })
 export class SidebarComponent {
-  open: boolean = false;
+
+
 
 }
--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LevelDataService } from 'src/app/services/DataLevels/level-data.service';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+
+  levels: any[] = [];
+
+  totalLevels: number = 0;
+
+  constructor(private levelDataService: LevelDataService) {}
+
+  ngOnInit(): void {
+    // this.getStatisticsLevels();
+    this.calculateTotalLevels()
+  }
+
+  // getStatisticsLevels() {
+  //   this.levelDataService.levels$.subscribe(levels => {
+  //     this.levels = levels;
+  //     console.log(levels)
+  //   });
+  // }
+
+
+  calculateTotalLevels() {
+    this.totalLevels = this.levelDataService.getTotalLevels();
+    console.log('Total levels 11233:', this.totalLevels);
+  }
 
 }
